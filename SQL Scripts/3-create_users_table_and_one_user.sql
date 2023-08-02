00bd01132f8c02bef9bb7563dab6295593d82c53
Pedro Costa <pedro.da-costa@capgemini.com>
2023-08-02 12:10:30 +0100

    Missing Authorities table

--- SQL Scripts/3-create_users_table_and_one_user.sql
+++ SQL Scripts/3-create_users_table_and_one_user.sql
@@ -12,3 +12,14 @@ create table users (
 -- pedro - default
 insert into users
 values ('pedro','{bcrypt}$2y$10$zkAxlVrud.lznJQVz2jQz.QhTfKedVfW.blHFP60a.Tp4Lb6p4rZy',1);
+
+-- Create authorities tables
+create table authorities (
+  username varchar(50) not null,
+  authority varchar(50) not null,
+  unique key userAuth_uni (username ,authority),
+  foreign key (username) references users(username)
+);
+
+insert into authorities 
+values ('pedro','ROLE_ADMIN');
